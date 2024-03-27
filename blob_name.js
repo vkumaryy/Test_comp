c3e609dfa00b284a8f37f10f27398bf9423d8f3f
Vikash kumar <vkumaryy@gmail.com>
2024-03-27 15:45:02 +0530

    blob_name.js

---
+++ blob_name.js
@@ -0,0 +1,3 @@
+const url = "https://igtstore.blob.core.windows.net/igtcontainer/temp/TATATELE INVOICE3.pdf";
+const blobName = url.substring(url.lastIndexOf("/") + 1);
+console.log(blobName);
---
+++ blob_name.py
@@ -0,0 +1,3 @@
+url = "https://igtstore.blob.core.windows.net/igtcontainer/temp/TATATELE INVOICE3.pdf"
+blob_name = url.split("/")[-1]
+print(blob_name)
